02b49b042f62a0650e127f85adbfda2a930a3a11
Kenza Mahresi <k.mahresi@gmail.com>
2020-05-15 15:12:28 +0200

    update

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,4 @@
 class CocktailsController < ApplicationController
-
   def index
     @cocktails = Cocktail.all
   end
@@ -18,14 +17,8 @@ class CocktailsController < ApplicationController
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'new'
     end
-  end
-
-  def destroy
-    @cocktail = Cocktail.find(params[:id])
-    @cocktail.destroy
-    redirect_to cocktail_path
   end
 
   private
@@ -34,3 +27,40 @@ class CocktailsController < ApplicationController
     params.require(:cocktail).permit(:name)
   end
 end
+
+# class CocktailsController < ApplicationController
+
+#   def index
+#     @cocktails = Cocktail.all
+#   end
+
+#   def show
+#     @cocktail = Cocktail.find(params[:id])
+#     @dose = Dose.new
+#   end
+
+#   def new
+#     @cocktail = Cocktail.new
+#   end
+
+#   def create
+#     @cocktail = Cocktail.new(cocktail_params)
+#     if @cocktail.save
+#       redirect_to cocktail_path(@cocktail)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#     @cocktail = Cocktail.find(params[:id])
+#     @cocktail.destroy
+#     redirect_to cocktail_path
+#   end
+
+#   private
+
+#   def cocktail_params
+#     params.require(:cocktail).permit(:name)
+#   end
+# end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,7 +12,8 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render "cocktails/show"
     end
   end
 
@@ -22,9 +23,42 @@ class DosesController < ApplicationController
     redirect_to cocktail_path(@dose.cocktail)
   end
 
-   private
+  private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
 end
+
+# class DosesController < ApplicationController
+
+#   def new
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new
+#   end
+
+#   def create
+#     @cocktail = Cocktail.find(params[:cocktail_id])
+#     @dose = Dose.new(dose_params)
+#     @dose.cocktail = @cocktail
+#     if @dose.save
+#       redirect_to cocktail_path(@cocktail)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#     @dose = Dose.find(params[:id])
+#     @dose.destroy
+#     redirect_to cocktail_path(@dose.cocktail)
+#   end
+
+#    private
+
+#   def dose_params
+#     params.require(:dose).permit(:description, :ingredient_id)
+#   end
+# end
+
+
